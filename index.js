5d8eb352b1a413467df97d46d4b119bd98dd1a81
kartavya <kartavya.aggarwal07@gmail.com>
2026-05-17 17:55:47 +0530

    fix color change issue

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-const container = document.querySelector(".container");
-const btn = document.querySelector("#boxes");
+const container = document.querySelector('.container');
+const btn = document.querySelector('#boxes');
+let count = 0;
 
-for(let j = 0; j < 1; j++){
+for (let j = 0; j < 1; j++) {
   generateButtons(4);
 }
 
-btn.addEventListener("click", () => {
-  let response = prompt("Enter the size of grid (max 100)");
+btn.addEventListener('click', () => {
+  let response = prompt('Enter the size of grid (max 100)');
   const numResponse = parseInt(response);
-  if(numResponse <= 100){
+  if (numResponse <= 100) {
     count = numResponse;
   }
   container.replaceChildren();
   generateButtons(count);
 });
 
-function generateButtons(count){
-  for (let i = 0; i < (count*count); i++) {
-    const childDiv = document.createElement("div");
-    childDiv.classList.add("child-div");
+function generateButtons(count) {
+  for (let i = 0; i < count * count; i++) {
+    const childDiv = document.createElement('div');
+    childDiv.classList.add('child-div');
     childDiv.style.width = `${100 / count}%`;
+    childDiv.classList.add('new-style');
     container.appendChild(childDiv);
     
-    childDiv.addEventListener("mouseover", () => {
-      let color = getRandomRGB();
-      childDiv.style.backgroundColor = color;
+    let color = getRandomRGB();
+    childDiv.addEventListener('mouseover', () => {
+      if (childDiv.classList.contains('new-style')) {
+        childDiv.style.backgroundColor = color;
+        childDiv.classList.remove('new-style');
+      }
+      /*
+      count++;
+      if (count == 0) {
+        count++;
+      } else {
+        childDiv.style.opacity = 1;
+        console.log(count);
+      }
+      */
+      /*
+      let divOpacity = 0;
+      childDiv.style.opacity = (divOpacity + 0.1);
+      */
     });
   }
 }
 
-function getRandomRGB(){
+function getRandomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return `rgb(${r}, ${g}, ${b})`;
+  const a = 0.1;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
