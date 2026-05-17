6a6a2c4a1a5b3852293de1985aa6c83b23805e40
kartavya <kartavya.aggarwal07@gmail.com>
2026-05-17 15:15:39 +0530

    v1 complete

--- index.js
+++ index.js
@@ -2,26 +2,28 @@ const container = document.querySelector(".container");
 const btn = document.querySelector("#boxes");
 
 for(let j = 0; j < 1; j++){
-  generateButtons(16);
+  generateButtons(4);
 }
 
 btn.addEventListener("click", () => {
-  let response = prompt("Enter the number of boxes you want (max 100)");
+  let response = prompt("Enter the size of grid (max 100)");
   const numResponse = parseInt(response);
   if(numResponse <= 100){
     count = numResponse;
   }
+  container.replaceChildren();
   generateButtons(count);
 });
 
 function generateButtons(count){
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < (count*count); i++) {
     const childDiv = document.createElement("div");
     childDiv.classList.add("child-div");
+    childDiv.style.width = `${100 / count}%`;
     container.appendChild(childDiv);
     
     childDiv.addEventListener("mouseover", () => {
-      childDiv.style.backgroundColor = "skyblue";
+      childDiv.style.backgroundColor = "dodgerblue";
     });
   }
 }
